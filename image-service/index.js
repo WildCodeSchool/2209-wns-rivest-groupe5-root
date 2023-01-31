430c08511badaffa2e003a7d050b907cc15d88e1
Joel RAZAFINJATOVO <joel.razafinjatovo@epitech.eu>
2023-01-31 16:56:42 +0100

    add image uploader

---
+++ image-service/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const formidableMiddleware = require('express-formidable');
+require('dotenv').config()
+const cors = require("cors");
+const app = express()
+const port = 4040
+
+
+
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+app.use(cors());
+app.use(formidableMiddleware());
+
+app.get('/', (req, res) => {
+    res.send('Hello World picture uploader here!!!')
+})
+
+app.post('/upload', async (req, res) => {
+    try {
+        console.log(">>>>files received", req.files)
+        console.log(">>>>files received path ", req.files.file.path)
+        let pictureToUpload = req.files.file.path;
+        const result = await cloudinary.uploader.upload(pictureToUpload, { folder: 'wildcarbon/gooddeals' });
+        console.log(">>>>>response from cloudinary>>>", result)
+        return res.status(200).json(result);
+    } catch (error) {
+        console.log(">>>ERROR>>>", error.message)
+        return res.status(400).json({ error: error.message });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
